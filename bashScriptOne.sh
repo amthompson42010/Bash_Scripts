746d629f58ddb54136d6dc51d99a5982128e7a11
Alexander Mark Thompson <amthompson42010@gmail.com>
2016-01-29 13:56:45 -0600

    Added two bash scripts that I created

---
+++ bashScriptOne.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+clear
+
+echo "The script starts now."
+
+echo "Hi, $USER!"
+echo
+
+echo "I will now fetch you a list of connected users:"
+echo
+w
+echo
+
+echo "I'm giving you back your prompt now."
+echo
